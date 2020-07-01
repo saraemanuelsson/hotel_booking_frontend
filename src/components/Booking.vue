f241245e068d1423fc34fa3e8ba4a787611ab2cc
Sara Emanuelsson <sara256emanuelsson@gmail.com>
2020-07-01 15:34:06 +0100

    Add styling

--- src/components/Booking.vue
+++ src/components/Booking.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="box">
-    <div>
-        <label>Name:</label>
+    <div id="name">
+        <label>Name: </label>
         <EditableText :value="booking.name" v-on:edited="editedName"/>
     </div>
-    <div>
-        <label>Email:</label>
-        <EditableText :value="booking.email" v-on:edited="editedEmail"/>
-    </div>
-    <div>
+    <div id="checked-in">
         <label for="checkbox">Checked In:</label>
         <input type="checkbox" v-model="booking.checkedIn" v-on:change="updateBooking" id="checkbox">
     </div>
-    <button name="delete" v-on:click="deleteBooking">Delete</button>
+    <div id="email">
+        <label>Email: </label>
+        <EditableText :value="booking.email" v-on:edited="editedEmail"/>
+    </div>
+    <button id="delete-button" name="delete" v-on:click="deleteBooking">Delete</button>
   </div>
 </template>
 
@@ -54,6 +54,28 @@ export default {
 
 <style>
 .box {
-    border: 1px solid black;
+    border: 1px solid white;
+    margin: 1em;
+    display: grid;
+    grid-template-columns: 5fr 1fr;
+    padding: 5px;
 }
+
+#checked-in {
+    justify-self: right;
+}
+
+#delete-button {
+    width: fit-content;
+    justify-self: right;
+    background: rgba(220, 20, 60, 0.603);
+    color: white;
+}
+
+label {
+    display: inline-block;
+    font-weight: bold;
+    margin: 0px 5px 0px 0px;
+}
+
 </style>
--- src/components/BookingForm.vue
+++ src/components/BookingForm.vue
@@ -1,56 +1,84 @@
 <template>
-  <form v-on:submit="addBooking">
-    <div class="a">
-        <label for="name">Name:</label>
-        <input type="text" id="name" v-model="name">
-    </div>
-
-    <div class="a">
-        <label for="email">Email:</label>
-        <input type="email" id="email" v-model="email">
+<div class="booking-box">
+  <form id="new-booking" v-on:submit="addBooking">
+    <h2>New Booking</h2>
+    <div>
+      <label for="name">Name:</label>
+      <input type="text" id="name" v-model="name" />
     </div>
 
     <div>
-        <label for="checked-in">Checked In:</label>
-        <input type="checkbox" id="checked-in" v-model="checkedIn">
+      <label for="email">Email:</label>
+      <input type="text" id="email" v-model="email" />
     </div>
 
-    <div>
-        <button type="submit">Send</button>
+    <div class="cell">
+        <div>
+            <label for="check">Checked In:</label>
+            <input type="checkbox" id="check" v-model="checkedIn" />
+        </div>
+      <input id="button" type="submit" />
     </div>
   </form>
+  </div>
 </template>
 
 <script>
-import { eventBus } from "../main.js"
-import BookingsService from "../services/BookingsService.js"
-
+import { eventBus } from "../main.js";
+import BookingsService from "../services/BookingsService.js";
 
 export default {
-    name: "booking-form",
-    data() {
-        return {
-            name: "",
-            email: "",
-            checkedIn: false
-        }
-    },
-    methods: {
-        addBooking(e) {
-            e.preventDefault()
-            const booking = {
-                name: this.name,
-                email: this.email,
-                checkedIn: this.checkedIn
-            }
-            BookingsService.postBooking(booking)
-            .then(res => eventBus.$emit("booking-added", res))
-            e.target.reset()
-        }
+  name: "booking-form",
+  data() {
+    return {
+      name: "",
+      email: "",
+      checkedIn: false
+    };
+  },
+  methods: {
+    addBooking(e) {
+      e.preventDefault();
+      const booking = {
+        name: this.name,
+        email: this.email,
+        checkedIn: this.checkedIn
+      };
+      BookingsService.postBooking(booking).then(res =>
+        eventBus.$emit("booking-added", res)
+      );
+      e.target.reset();
     }
-}
+  }
+};
 </script>
 
 <style>
+#new-booking {
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-gap: 12px;
+  align-items: center;
+  justify-items: center;
+}
+
+.booking-box {
+    border: 1px solid white;
+    margin: 1em;
+}
+
+.cell {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  margin-bottom: 1em;
+}
+.cell input[type="text"] {
+  flex: 1;
+}
+
+#button {
+    margin: 0 auto;
+    width: 80%;
+}
 
 </style>
